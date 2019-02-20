41f73cb5cdb8786535539224869ee03a8cf7b9e6
Adrianna Frick <47511437+adrifrick@users.noreply.github.com>
2019-02-19 22:25:18 -0800

    Create Inventory.R

---
+++ Inventory.R
@@ -0,0 +1,24 @@
+# Adri Frick's Inventory Analysis Project
+# Question: How long does product stay on shelf until sold out?
+# Output: graph of date ranges
+library(lubridate)
+library(ggplot2)
+
+# Step 1: Get Inventory & Set Headers
+inventory.csv <- read.csv("C:/Users/Adri/Downloads/Inventory Info.csv", header=TRUE)
+names(inventory.csv) <- c("Name", "Qty" , "Price" , "Sold" , "Purchased")
+
+#Step 2: Ignore observations with >=1 value in Qty column
+inventory.sold <- subset (inventory.csv, Qty >= 1)
+
+#Step 3: Format Data
+date.sold <- parse_date_time(inventory.sold$Sold, orders = "mdy_HM")
+date.purchased <- parse_date_time(inventory.sold$Purchased, orders = "mdy_HM")
+
+#Step 4: Calculate difference between C and D for each item
+unsold.weeks <- difftime (date.sold,date.purchased, , units = "weeks")
+item.name <- lapply (inventory.sold [1], as.character)
+
+inventory.report <- data.frame(item.name, date.purchased, date.sold, unsold.weeks)
+
+print(inventory.report)
