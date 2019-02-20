acdaff9d7e873a09dd7ae5394038b82e4c8b123b
Adrianna Frick <47511437+adrifrick@users.noreply.github.com>
2019-02-20 06:54:28 -0800

    Fixed comments to match Google Style Guide

--- Inventory.R
+++ Inventory.R
@@ -7,20 +7,20 @@ library(lubridate)
 inventory.csv <- read.csv("C:/Users/Adri/Downloads/Inventory Info.csv", header=TRUE)
 names(inventory.csv) <- c("Name", "Qty" , "Price" , "Sold" , "Purchased")
 
-#Step 2: Ignore observations with >=1 value in Qty column
+# Step 2: Ignore observations with >=1 value in Qty column
 inventory.sold <- subset (inventory.csv, Qty <= 0)
 
-#Step 3: Format Data
+# Step 3: Format Data
 date.sold <- parse_date_time(inventory.sold$Sold, orders = "mdy_HM")
 date.purchased <- parse_date_time(inventory.sold$Purchased, orders = "mdy_HM")
 
-#Step 4: Calculate difference between C and D for each item
+# Step 4: Calculate difference between C and D for each item
 unsold.weeks <- difftime (date.sold,date.purchased, , units = "weeks")
 item.name <- lapply (inventory.sold [1], as.character)
 
 inventory.report <- data.frame(item.name, date.purchased, date.sold, unsold.weeks)
 
-#Step 5: Output
+# Step 5: Output
 print(inventory.report)
 
 plot(date.sold, type="l")
